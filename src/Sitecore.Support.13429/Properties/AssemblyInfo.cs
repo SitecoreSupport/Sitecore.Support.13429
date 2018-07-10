5ebbedf738d1152f6692324e951bd49b3cd096f3
vle <vle@sitecore.net>
2018-07-10 18:21:43 +0300

    fix

---
+++ src/Sitecore.Support.13429/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.13429")]
+[assembly: AssemblyProduct("Sitecore.Support.13429")]
+[assembly: ComVisible(false)]
